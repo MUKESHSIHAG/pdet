4d3890b31663734769789e10997e956956740048
mukeshsihag <sihagmukesh22@gmail.com>
2021-03-05 01:56:17 +0530

    api requirements fixing

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from app import app
 from flask import request, jsonify
 
-@app.route('/get-result', methods=['GET'])
+@app.route('/')
 def index():
-    print(request.args.get("ax"),request.args.get("az"))
+    return "Pdet api is working properly"
+
+@app.route('/get_result', methods=['GET'])
+def get_result():
     res = 'Good posture'
     if request.args.get("ax")==request.args.get("ay")==request.args.get("az"):
         res="Bad posture"
